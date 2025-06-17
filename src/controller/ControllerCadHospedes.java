76772fc43a368060cb2705b2048045343b34d56e
mirelasc23 <mirelamsccomach@gmail.com>
2025-06-17 19:25:25 -0300

    insere classe ControllerCadHospedes

---
+++ src/controller/ControllerCadHospedes.java
@@ -0,0 +1,13 @@
+package controller;
+
+import view.CadastroHospedes;
+
+public class ControllerCadHospedes {
+    private CadastroHospedes telaCadastroHospedes;
+
+    public ControllerCadHospedes(CadastroHospedes telaCadastroHospedes) {
+        this.telaCadastroHospedes = telaCadastroHospedes;
+    }
+    
+    
+}
