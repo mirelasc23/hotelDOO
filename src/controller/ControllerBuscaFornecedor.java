e7f7634a92f5ec94f90526e78f7d7807050e8a62
mirelasc23 <mirelamsccomach@gmail.com>
2025-07-08 05:25:16 -0300

    inicia ControllerBuscaFuncionario

---
+++ src/controller/ControllerBuscaFornecedor.java
@@ -0,0 +1,13 @@
+package controller;
+
+import view.BuscaFornecedor;
+
+public class ControllerBuscaFornecedor {
+    private BuscaFornecedor telaBuscaFornecedor;
+
+    public ControllerBuscaFornecedor(BuscaFornecedor telaBuscaFornecedor) {
+        this.telaBuscaFornecedor = telaBuscaFornecedor;
+    }
+    
+    
+}
