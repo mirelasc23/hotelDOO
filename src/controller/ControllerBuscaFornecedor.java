04161c64b5b5a89486f6412ab93e91763a917cc9
mirelasc23 <mirelamsccomach@gmail.com>
2025-07-08 05:30:16 -0300

    configura ControllerBuscaFornecedor

--- src/controller/ControllerBuscaFornecedor.java
+++ src/controller/ControllerBuscaFornecedor.java
@@ -1,12 +1,20 @@
 package controller;
 
+import java.awt.event.ActionListener;
 import view.BuscaFornecedor;
 
-public class ControllerBuscaFornecedor {
+public class ControllerBuscaFornecedor implements ActionListener{
     private BuscaFornecedor telaBuscaFornecedor;
 
     public ControllerBuscaFornecedor(BuscaFornecedor telaBuscaFornecedor) {
         this.telaBuscaFornecedor = telaBuscaFornecedor;
+        
+        this.telaBuscaFornecedor.getjButtonCarregar().addActionListener(this);
+        this.telaBuscaFornecedor.getjButtonFechar().addActionListener(this);
+        this.telaBuscaFornecedor.getjButtonFiltrar().addActionListener(this);
+        
+        utilities.Utilities.ativaDesativaBotoes(this.telaBuscaFornecedor.getj, true);
+        
     }
     
     
