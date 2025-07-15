a862c0bfa62a1289a7c0e41120204226d6af0b2e
mirelasc23 <mirelamsccomach@gmail.com>
2025-07-15 06:37:08 -0300

    adiciona controllerCad faltantes

--- src/controller/ControllerCadModelo.java
+++ src/controller/ControllerCadModelo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.awt.event.ActionEvent;
@@ -10,10 +5,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 import view.CadastroModelo;
 
-/**
- *
- * @author Info
- */
 public class ControllerCadModelo implements ActionListener{
     private CadastroModelo telaCadastroModelo;
 
