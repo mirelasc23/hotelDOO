74d439edef8b781fb31133d58f43e7e33e9b808a
mirelasc23 <mirelamsccomach@gmail.com>
2025-08-25 21:10:14 -0300

    dao vaga e servico

---
+++ src/model/dao/ServicoDAO.java
@@ -0,0 +1,33 @@
+package model.dao;
+
+import java.util.List;
+import model.Servico;
+
+public class ServicoDAO implements InterfaceDAO<Servico>{
+
+    @Override
+    public void create(Servico objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Servico retrieve(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Servico> retrieve(String atributo, String valor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Servico objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Servico objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
