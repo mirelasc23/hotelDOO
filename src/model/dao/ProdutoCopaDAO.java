155923003e4c5b037b83fec20feb037ba909d995
mirelasc23 <mirelamsccomach@gmail.com>
2025-08-28 11:51:54 -0300

    dao reserva, produtoCopa, ordemServico e copaQuarto

---
+++ src/model/dao/ProdutoCopaDAO.java
@@ -0,0 +1,33 @@
+package model.dao;
+
+import java.util.List;
+import model.ProdutoCopa;
+
+public class ProdutoCopaDAO implements InterfaceDAO<ProdutoCopa>{
+
+    @Override
+    public void create(ProdutoCopa objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ProdutoCopa retrieve(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<ProdutoCopa> retrieve(String atributo, String valor) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(ProdutoCopa objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(ProdutoCopa objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
