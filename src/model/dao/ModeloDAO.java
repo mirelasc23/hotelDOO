e210ba3e0c7618608f344ac950d96760509c6007
mirelasc23 <mirelamsccomach@gmail.com>
2025-10-15 13:22:35 -0300

    atualiza

--- src/model/dao/ModeloDAO.java
+++ src/model/dao/ModeloDAO.java
@@ -1,13 +1,35 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 import model.Modelo;
 
 public class ModeloDAO implements InterfaceDAO<Modelo>{
 
     @Override
     public void create(Modelo objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sqlInstrucao = "insert into modelo (descricao, marca_id) values(?)";
+        
+        Connection conexao = ConnectionFactoty.getConnection();    
+        PreparedStatement pstm = null;
+        
+        try {        
+            pstm = conexao.prepareStatement(sqlInstrucao);
+            
+            pstm.setString(1, objeto.getDescricao());
+            
+            JOptionPane.showMessageDialog(null, objeto);
+            pstm.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally{
+            ConnectionFactoty.closeConnecition(conexao, pstm);
+        }
     }
 
     @Override
