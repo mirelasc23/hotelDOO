d153456a57d994204869056d5c644599775eb3e2
mirelasc23 <mirelamsccomach@gmail.com>
2025-10-29 23:31:13 -0300

    atualiza produtoCopaDAO

--- src/service/ProdutoCopaService.java
+++ src/service/ProdutoCopaService.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package service;
 
-/**
- *
- * @author Info
- */
-public class ProdutoCopaService {
+import java.util.List;
+import model.ProdutoCopa;
+import model.dao.ProdutoCopaDAO;
+
+public class ProdutoCopaService implements InterfaceService<ProdutoCopa>{
+
+    @Override
+    public void Criar(ProdutoCopa objeto) {
+        ProdutoCopaDAO produtoCopaDAO = new ProdutoCopaDAO();
+        produtoCopaDAO.create(objeto);
+    }
+
+    @Override
+    public ProdutoCopa Carregar(int id) {
+        ProdutoCopaDAO produtoCopaDAO = new ProdutoCopaDAO();
+        return produtoCopaDAO.retrieve(id);
+    }
+
+    @Override
+    public List<ProdutoCopa> Carregar(String atributo, String valor) {
+        ProdutoCopaDAO produtoCopaDAO = new ProdutoCopaDAO();
+        return produtoCopaDAO.retrieve(atributo, valor);
+    }
+
+    @Override
+    public void Atualizar(ProdutoCopa objeto) {
+        ProdutoCopaDAO produtoCopaDAO = new ProdutoCopaDAO();
+        produtoCopaDAO.update(objeto);
+    }
+
+    @Override
+    public void Deletar(ProdutoCopa objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
